6cfac0bd52a22b4f172b37faf63c111d4f9b5300
damo1306 <damo130685@outlook.com>
2024-12-08 01:29:39 -0600

    chore: prueba de ejemplo con código para validar commit

---
+++ SDK/Comercial/C#/Ejemplos/Conector/inicioComercialSDK_CSharp/inicioComercialSDK/EntradaCompra.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace inicioComercialSDK
+{
+    partial class EntradaCompra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigoProveedor = new System.Windows.Forms.TextBox();
+            this.txtCodigoProducto = new System.Windows.Forms.TextBox();
+            this.txtNombreProveedor = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.btnBuscaProveedor = new System.Windows.Forms.Button();
+            this.btnBuscaProducto = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiarCampos = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDocumentoGenerado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código Proveedor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Código Producto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cantidad";
+            // 
+            // txtCodigoProveedor
+            // 
+            this.txtCodigoProveedor.Location = new System.Drawing.Point(170, 23);
+            this.txtCodigoProveedor.Name = "txtCodigoProveedor";
+            this.txtCodigoProveedor.Size = new System.Drawing.Size(100, 22);
+            this.txtCodigoProveedor.TabIndex = 3;
+            // 
+            // txtCodigoProducto
+            // 
+            this.txtCodigoProducto.Location = new System.Drawing.Point(170, 70);
+            this.txtCodigoProducto.Name = "txtCodigoProducto";
+            this.txtCodigoProducto.Size = new System.Drawing.Size(100, 22);
+            this.txtCodigoProducto.TabIndex = 4;
+            // 
+            // txtNombreProveedor
+            // 
+            this.txtNombreProveedor.Location = new System.Drawing.Point(301, 23);
+            this.txtNombreProveedor.Name = "txtNombreProveedor";
+            this.txtNombreProveedor.ReadOnly = true;
+            this.txtNombreProveedor.Size = new System.Drawing.Size(276, 22);
+            this.txtNombreProveedor.TabIndex = 5;
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(301, 70);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.ReadOnly = true;
+            this.txtNombreProducto.Size = new System.Drawing.Size(276, 22);
+            this.txtNombreProducto.TabIndex = 6;
+            // 
+            // btnBuscaProveedor
+            // 
+            this.btnBuscaProveedor.Location = new System.Drawing.Point(615, 19);
+            this.btnBuscaProveedor.Name = "btnBuscaProveedor";
+            this.btnBuscaProveedor.Size = new System.Drawing.Size(142, 30);
+            this.btnBuscaProveedor.TabIndex = 7;
+            this.btnBuscaProveedor.Text = "Busca Proveedor";
+            this.btnBuscaProveedor.UseVisualStyleBackColor = true;
+            this.btnBuscaProveedor.Click += new System.EventHandler(this.btnBuscaProveedor_Click);
+            // 
+            // btnBuscaProducto
+            // 
+            this.btnBuscaProducto.Location = new System.Drawing.Point(615, 66);
+            this.btnBuscaProducto.Name = "btnBuscaProducto";
+            this.btnBuscaProducto.Size = new System.Drawing.Size(142, 30);
+            this.btnBuscaProducto.TabIndex = 8;
+            this.btnBuscaProducto.Text = "Busca Producto";
+            this.btnBuscaProducto.UseVisualStyleBackColor = true;
+            this.btnBuscaProducto.Click += new System.EventHandler(this.btnBuscaProducto_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(29, 247);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(106, 30);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiarCampos
+            // 
+            this.btnLimpiarCampos.Location = new System.Drawing.Point(471, 247);
+            this.btnLimpiarCampos.Name = "btnLimpiarCampos";
+            this.btnLimpiarCampos.Size = new System.Drawing.Size(106, 30);
+            this.btnLimpiarCampos.TabIndex = 10;
+            this.btnLimpiarCampos.Text = "LIMPIAR";
+            this.btnLimpiarCampos.UseVisualStyleBackColor = true;
+            this.btnLimpiarCampos.Click += new System.EventHandler(this.btnLimpiarCampos_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Precio";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(651, 247);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(106, 30);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(170, 120);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(70, 22);
+            this.txtCantidad.TabIndex = 13;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(170, 179);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(70, 22);
+            this.txtPrecio.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(373, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Documento Generado";
+            // 
+            // txtDocumentoGenerado
+            // 
+            this.txtDocumentoGenerado.Location = new System.Drawing.Point(533, 176);
+            this.txtDocumentoGenerado.Name = "txtDocumentoGenerado";
+            this.txtDocumentoGenerado.ReadOnly = true;
+            this.txtDocumentoGenerado.Size = new System.Drawing.Size(44, 22);
+            this.txtDocumentoGenerado.TabIndex = 16;
+            // 
+            // EntradaCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 313);
+            this.Controls.Add(this.txtDocumentoGenerado);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnLimpiarCampos);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBuscaProducto);
+            this.Controls.Add(this.btnBuscaProveedor);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.txtNombreProveedor);
+            this.Controls.Add(this.txtCodigoProducto);
+            this.Controls.Add(this.txtCodigoProveedor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EntradaCompra";
+            this.Text = "EntradaCompra";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigoProveedor;
+        private System.Windows.Forms.TextBox txtCodigoProducto;
+        private System.Windows.Forms.TextBox txtNombreProveedor;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.Button btnBuscaProveedor;
+        private System.Windows.Forms.Button btnBuscaProducto;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiarCampos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDocumentoGenerado;
+    }
+}
---
+++ SDK/Comercial/C#/Ejemplos/Conector/inicioComercialSDK_CSharp/inicioComercialSDK/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inicioComercialSDK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CenterToScreen();
+            btnAbreEmpresa.Enabled = false;
+            btnCierraEmpresa.Enabled = false;
+            btnCartaPorte.Enabled = false;
+            btnEntradaCompra.Enabled = false;
+            btnTerminaSDK.Enabled = false;
+        }
+
+        private bool btnIniciaPress = false;
+        private void btnIniciaSDK_Click(object sender, EventArgs e)
+        {
+            btnIniciaPress = true;
+            SDK.fconexion(e);
+            MessageBox.Show("SDK Iniciado");
+            cmbEmpresas.DataSource = SDK.lista_empresas();
+            btnTerminaSDK.Enabled = true;
+            btnAbreEmpresa.Enabled = true;
+        }
+
+        private void btnTerminaSDK_Click(object sender, EventArgs e)
+        {
+            if (btnIniciaPress == true)
+            {
+                SDK.fCierraEmpresa();
+                SDK.fTerminaSDK();
+                MessageBox.Show("Aplicación SDK Finalizada..");
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("No iniciaste SDK");
+                Close();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (btnIniciaPress)
+            {
+                SDK.fCierraEmpresa();
+                SDK.fTerminaSDK();
+            }                                                             
+        }
+
+        private void btnAbreEmpresa_Click(object sender, EventArgs e)
+        {
+            if (btnIniciaPress)
+            {
+                Object selecItem = cmbEmpresas.SelectedItem;
+                int abreEmpresa = SDK.fAbreEmpresa(selecItem.ToString());
+
+                if(abreEmpresa != 0)
+                {
+                    MessageBox.Show("Error: " + SDK.rError(abreEmpresa));
+                }
+                else
+                {
+                    MessageBox.Show("Se ha abierto la empresa " + selecItem.ToString());
+                    btnCierraEmpresa.Enabled = true;
+                    btnCartaPorte.Enabled = true;
+                    btnEntradaCompra.Enabled = true;
+                }
+
+                
+            }
+            else
+            {
+                MessageBox.Show("Primero inicia SDK");
+            }
+        }
+
+        private void btnEntradaCompra_Click(object sender, EventArgs e)
+        {
+            EntradaCompra enCom = new EntradaCompra();
+            enCom.Show();
+        }
+
+        private void btnCierraEmpresa_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se cerrará la empresa " + cmbEmpresas.SelectedItem);
+            SDK.fCierraEmpresa();
+            cmbEmpresas.Text = "";
+            btnCierraEmpresa.Enabled = false;
+        }
+    }
+}
